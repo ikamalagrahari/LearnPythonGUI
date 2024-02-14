b7f3084fd5e193fa99377eb94fb97ade06c9f37c
Kamal Agrahari <kamalagrahari2003@gmail.com>
2024-02-14 20:40:57 +0530

    added eg

--- canvas.py
+++ canvas.py
@@ -1,1 +1,25 @@
+try:
+    from Tkinter import *
+except ImportError:
+    from tkinter import *
 
+try:
+    import ttk
+    py3 = False
+except ImportError:
+    import tkinter.ttk as ttk
+    py3 = True
+
+    root = Tk()
+
+canvas = Canvas(root)
+canvas.place(relx=0.01, rely=0.01, relheight=1, relwidth=1)
+canvas.configure(background="yellow")
+canvas.configure(height=150)
+canvas.configure(width=150)
+
+coord = 10,20,240,210
+arc = canvas.create_arc(coord, start=0, extent=150, fill="red")
+text = canvas.create_text(100,10,fill="darkblue",font="Times 20 italic bold",text=" Python GUI ")
+root.title("Canvas Example")
+root.mainloop()
